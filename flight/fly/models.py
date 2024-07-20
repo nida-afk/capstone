dfbfa51d20ddc326f2b8e46570dc68ba2ee51b75
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-20 14:22:28 +0000

    Sat, Jul 20, 2024, 7:52 PM +05:30

--- flight/fly/models.py
+++ flight/fly/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
 # Create your models here.
-
 class Jun(models.Model):
     name = models.CharField(max_length= 100)
 
-    def__str__(self):
+    def __str__(self):
         return  f"{self.name}"
 
 class Fly(models.Model):
     bird = models.CharField(max_length= 100)
-    forest = models.
+    forest = models.ForeignKey(Jun, on_delete= models.CASCADE,  related_name= "forest")
+
+    def __str__(self):
+        return f"{self.bird} lives in {self.forest}"
