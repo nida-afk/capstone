ecd52e487b2f1e2deb32d7ed3ce9b98d83abe5b3
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-20 14:17:18 +0000

    Sat, Jul 20, 2024, 7:47 PM +05:30

--- flight/fly/models.py
+++ flight/fly/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Jun(models.Model):
+    name = models.CharField(max_length= 100)
+
+    def__str__(self):
+        return  f"{self.name}"
+
 class Fly(models.Model):
     bird = models.CharField(max_length= 100)
     forest = models.
