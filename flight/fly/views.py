8cfcf2c805b20321c762b808853e59e065179e81
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-21 10:59:10 +0000

    Sun, Jul 21, 2024, 4:29 PM +05:30

--- flight/fly/views.py
+++ flight/fly/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from .models import Fly, Jun
+from django.
 
 # Create your views here.
 def index(request):
-    return (render(request, index.html, {
-        
-    }))
+    return HttpResponse("")
