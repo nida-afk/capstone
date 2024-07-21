2a513f769f5d450ba3144e49c5d6991295ac1e34
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-21 12:13:48 +0000

    Sun, Jul 21, 2024, 5:43 PM +05:30

--- flight/fly/views.py
+++ flight/fly/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     f = Fly.objects.all()
-    resp = ([f"<li>  {d.id}  {d} <li>" for d in f])  # Joining each entry with an HTML line break
-
+    "<ol>"
+    resp = [f" {d.id}  <li> {d} </li>" for d in f] # Joining each entry with an HTML line break
+    "<ol>"
     return HttpResponse(resp)
