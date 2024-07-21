dbab6c7722c35bc7687a83ecf2100df31c4f1e50
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-21 11:35:21 +0000

    Sun, Jul 21, 2024, 5:05 PM +05:30

--- flight/fly/views.py
+++ flight/fly/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     response_text = ""
-    for i in range(0, 7):
+    for i in range(1, 7):
+        f = Fly.objects.filter
         response_text += f"Hello {i} "
     return HttpResponse(response_text)
