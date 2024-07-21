d3c6bbee24eb640f4de0687c0c4b3a65b45ef3d4
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-21 11:21:41 +0000

    Sun, Jul 21, 2024, 4:51 PM +05:30

--- flight/fly/views.py
+++ flight/fly/views.py
@@ -4,5 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-
-    return HttpResponse("Hello")
+    for i in range (7):
+        return HttpResponse("Hello "+ i)
