f31b8d5739ee8228d19a900c280e445d38911136
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-22 09:22:16 +0000

    Mon, Jul 22, 2024, 2:52 PM +05:30

--- flight/fly/models.py
+++ flight/fly/models.py
@@ -7,14 +7,14 @@ class Jun(models.Model):
     def __str__(self):
         return  f"{self.name}"
 
+class Name(models.Model):
+    i = models.CharField(max_length= 100)
+    def __str__(self):
+        return f"{self.i}"
+
 class Fly(models.Model):
     bird = models.CharField(max_length= 100)
     forest = models.ForeignKey(Jun, on_delete= models.CASCADE,  related_name= "forest")
     name = models.ManyToManyField(Name,  related_name= "name")
     def __str__(self):
-        return f"{self.bird} lives in {self.forest}"
-
-class Name(models.Model):
-    i = models.CharField(max_length= 100)
-    def __str__(self):
-        return f"{self.i}"
+        return f"{self.bird} lives in {self.forest} his name is {self.name}"
