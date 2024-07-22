e14ff0b726e8e6f771ec8dd63ebf763274f394f6
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-22 12:37:27 +0000

    Mon, Jul 22, 2024, 6:07 PM +05:30

--- flight/fly/views.py
+++ flight/fly/views.py
@@ -9,11 +9,12 @@ def index(request):
     resp = [f"<li type =none> {d.id}  {d} </li>" for d in f] # Joining each entry with an HTML line break
     return HttpResponse(resp)
 
-def flight(request, i){
+def flight(request, i):
+
     f = Fly.objects.get(pk=i)
     return render(request, "fly.html", {
             "fly" : f.name.all(),
-            "flys" : f
+            "f" : f
     } )
 
-}
+
