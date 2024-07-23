f10e81c96f1c4f52efdbeb5a1662fc8cf42b1f69
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-23 12:40:19 +0000

    Tue, Jul 23, 2024, 6:10 PM +05:30

--- flight/fly/views.py
+++ flight/fly/views.py
@@ -25,6 +25,6 @@ def search(request):
             if s == i.name:
                 j = Jun.objects.get(name = s)
                 d = j.forest.all()
-    return render(request, "search.html", {
+        return render(request, "search.html", {
         "data" : d
     })
