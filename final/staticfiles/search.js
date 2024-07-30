fea72ce098826060a937d9ffabbb4a6addbd0878
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-07-30 09:34:33 +0000

    Tue, Jul 30, 2024, 3:04 PM +05:30

---
+++ final/staticfiles/search.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const Rows = document.querySelectorAll('.song-row');
+
+    Rows.forEach((row) => {
+        row.addEventListener('mouseenter', () => {
+            row.style.overflowX = 'auto';
+        });
+
+        row.addEventListener('mouseleave', () => {
+            row.style.overflowX = 'hidden';
+        });
+    });
+} )
+
+
+document.getElementById('searchInput').addEventListener('keyup', function (event) {
+
+    const search = event.target.value;
+    tunes(search);
+
+});
+
+function tunes(term) {
+const url = `https://itunes.apple.com/search?term=${term}&media=music&entity=song&limit=10`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(data => {
+        const results = document.getElementById('results');
+        results.innerHTML = '';
+        data.results.forEach(song => {
+            const songname = song.trackName;
+            const songele = document.createElement('div');
+            songele.innerHTML = `<p >${songname} </p>`;
+            results.appendChild(songele);
+
+
+
+        });
+    })
+    .catch(error => console.error('Error ', error));
+}
