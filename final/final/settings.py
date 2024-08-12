4948a187bf6386dce91bb1aa2afdb4759b3c9c08
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-08-12 09:36:51 +0000

    Mon, Aug 12, 2024, 3:06 PM +05:30

--- final/final/settings.py
+++ final/final/settings.py
@@ -9,10 +9,9 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-import dj_database_url
+
 from pathlib import Path
 import os
-
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,14 +78,14 @@ WSGI_APPLICATION = "final.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-if not DEBUG:
-    DATABASES = {'default': dj_database_url.parse("postgresql://all_sw21_user:Ritc6JJhRkRnyEm6ipnyhV7RK1QqxBWC@dpg-cqssinij1k6c73fng7d0-a/all_sw21")}
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR /'db.sqlite3',
-        }}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
