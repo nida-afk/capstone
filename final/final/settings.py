99b95a71e633a13cf56fb0a77e5a5df7f180b24d
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-08-12 09:35:09 +0000

    Mon, Aug 12, 2024, 3:05 PM +05:30

--- final/final/settings.py
+++ final/final/settings.py
@@ -85,7 +85,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': 'db.sqlite3',
+            'NAME': BASE_DIR /'db.sqlite3',
         }}
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
