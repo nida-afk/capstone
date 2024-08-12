1c885f367795c8d81b0233aa2d44e85e9a6dd812
nida-afk <87021685+nida-afk@users.noreply.github.com>
2024-08-12 09:19:30 +0000

    Mon, Aug 12, 2024, 2:49 PM +05:30

--- final/final/settings.py
+++ final/final/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import dj_database_url
 from pathlib import Path
 import os
-import dj_database_url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
